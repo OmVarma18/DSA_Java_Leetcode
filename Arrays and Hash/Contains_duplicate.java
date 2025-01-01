951d321135064e8e821fe1f48ee29743d64a5b62
OmVarma9 <omvarma369@gmail.com>
2025-01-01 23:33:45 +0530

    Main code

--- Arrays and Hash/Contains_duplicate.java
+++ Arrays and Hash/Contains_duplicate.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> num1= new HashSet<>();
@@ -7,5 +8,13 @@ class Solution {
             num1.add(num);
         }
         return false;
+    }
+}
+
+public class Contains_duplicate {
+    public static void main(String[] args) {
+        Solution s1=new Solution();
+        int[] nums={4,5,6,7,1};
+        s1.containsDuplicate(nums);
     }
 }
